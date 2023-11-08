c45f979bbb81f5cafaf0755e9304f8d7f437c168
Fábio <137011269+tuninof@users.noreply.github.com>
2023-11-08 16:36:33 -0300

    Update conexcao-bd.php

--- src/conexcao-bd.php
+++ src/conexcao-bd.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $dbname = "serenatto";
 $username = "root";
-$password = "bibilipe";
+$password = "";
 
 $pdo = new PDO("mysql:host=$host;dbname=$dbname",$username,$password);
 
